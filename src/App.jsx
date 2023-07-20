0c4a41f8dbc1912a546762c02e4fdca0ba66e4fd
Laura G <62339671+lpgiangrande@users.noreply.github.com>
2023-07-20 23:04:14 +0200

    1er jet ui

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+
+export default function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="App">
+        <div id='hello'>H E Y : )</div>
+        <div>
+          <img className="coffee" src="src/assets/images/coffee-mug.png" alt="coffee mug illustration" />
+        </div>
+        <Nav />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="" data-replace="Projects"><span>Projects</span></a></li>
+                    <li><a href="" data-replace="About"><span>About</span></a></li>
+                    <li><a href="" data-replace="Contact"><span>Contact</span></a></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+  )
+}
